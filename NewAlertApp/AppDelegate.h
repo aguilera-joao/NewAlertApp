6a3f3580dde88cf85317046f42b880af7c13483e
aguilera-joao <jg.alg34@gmail.com>
2015-04-17 16:46:49 -0700

    Initial Commit

---
+++ NewAlertApp/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  NewAlertApp
+//
+//  Created by iosDev on 4/17/15.
+//  Copyright (c) 2015 iosDev. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ NewAlertApp/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  NewAlertApp
+//
+//  Created by iosDev on 4/17/15.
+//  Copyright (c) 2015 iosDev. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
